76604a6ea181742faa8a1e3f9f84bd594909fcdb
pedrovernetti <pedrovernetti@gmail.com>
2020-07-06 02:43:18 -0300

    Installer improved

--- install.sh
+++ install.sh
@@ -9,6 +9,9 @@ tryDoing()
         fi
 }
 
+declare -r metanautilus_data_folder="/usr/share/metanautilus"
+declare -r extensions_folder="/usr/share/nautilus-python/extensions"
+
 # get the script's own path
 if [[ "$0" != /* ]]; then
     if [[ "$0" == './'* ]]; then declare -r selfpath="$PWD/${0#.\/}"
@@ -18,46 +21,72 @@ else
     declare -r selfpath="$0"
     fi
 
-declare -r data_folder="/usr/share/metanautilus"
-declare -r extensions_folder="/usr/share/nautilus-python/extensions"
-
-if [[ $(lsb_release -rs | head -c2) -lt 16 ]]; then
-    printf "\033[31;1mOld Ubuntu version (<16.04)...\033[0;0m\n"
-    exit
+# take command line arguments
+MODE=1
+if [[ "$#" -gt 1 ]]; then 
+    printf "Usage: '$0' [--reinstall|--uninstall|--full-uninstall]\n"
+    exit 1
+elif [[ "$#" -gt 0 ]]; then
+    if [[ "$1" == "--reinstall" ]]; then MODE=2
+    elif [[ "$1" == "--full-uninstall" ]]; then MODE=3
+    elif [[ "$1" == "--uninstall" ]]; then MODE=4
+    else printf "Usage: '$0' [--reinstall|--uninstall|--full-uninstall]\n"; fi
     fi
 
-printf "\033[1mInstalling dependencies...\033[0m\n"
-if [[ ( "$#" -ge 1 ) && ( "$1" =~ ^-?-(python)?3$ ) ]]; then 
-    tryDoing sudo apt -y install python3-pip; status=$?
-    pip='pip3'
+# default mode's first step (installing dependencies)
+if [[ "$MODE" -lt 2 ]]; then
+    if [[ $(lsb_release -rs | head -c2) -lt 16 ]]; then
+        printf "\033[31;1mOld Ubuntu version (<16.04)...\033[0;0m\n"
+        exit
+        fi
+    printf "\033[1mInstalling dependencies...\033[0m\n"
+    if [[ ( "$#" -ge 1 ) && ( "$1" =~ ^-?-(python)?3$ ) ]]; then 
+        tryDoing sudo apt -y install python3-pip; status=$?
+        pip='pip3'
+    else
+        tryDoing sudo apt -y install python-pip; status=$?
+        pip='pip'
+        fi
+    tryDoing sudo apt -y install python-nautilus
+    tryDoing sudo $pip install lxml
+    tryDoing sudo $pip install pymediainfo 
+    tryDoing sudo $pip install mutagen
+    tryDoing sudo $pip install pillow
+    if [[ "$pip" == 'pip3' ]]; then tryDoing sudo $pip install pyexiv2
+    else tryDoing sudo apt -y install python-pyexiv2; fi
+    tryDoing sudo $pip install pypdf2
+    tryDoing sudo $pip install olefile
+    tryDoing sudo $pip install torrentool
+    
+# non-default modes' first step (removing old files)
 else
-    tryDoing sudo apt -y install python-pip; status=$?
-    pip='pip'
+    sudo rm -vfr "$metanautilus_data_folder"
+    sudo rm -vf "$extensions_folder/metanautilus.py"
+    if [[ "$MODE" -lt 4 ]]; then rm -vfr "$HOME/.cache/metanautilus"
+    if [[ "$MODE" -gt 2 ]]; then 
+        # finishing
+        printf "\n\033[2m[press any key to restart Nautilus]\033[0m "; read -n 1 -s; printf "\n\n"
+        nautilus -q
+        nautilus &> /dev/null &
+        printf "\033[1;32mDONE!\033[0m\n"
+        exit
+        fi
     fi
-tryDoing sudo apt -y install python-nautilus
-tryDoing sudo $pip install lxml
-tryDoing sudo $pip install pymediainfo 
-tryDoing sudo $pip install mutagen
-tryDoing sudo $pip install pillow
-if [[ "$pip" == 'pip3' ]]; then tryDoing sudo $pip install pyexiv2
-else tryDoing sudo apt -y install python-pyexiv2
-tryDoing sudo $pip install pypdf2
-tryDoing sudo $pip install olefile
-tryDoing sudo $pip install torrentool
 
+# self-explainatory steps
 printf "\033[1mCopying mapping files to the (just created) data folder...\033[0m\n"
-tryDoing sudo mkdir -p "$data_folder"
-tryDoing sudo cp -i "${selfpath%/*}/"*.map "$data_folder"
+tryDoing sudo mkdir -p "$metanautilus_data_folder"
+tryDoing sudo cp -n "${selfpath%/*}/"*.map "$metanautilus_data_folder"
 
 printf "\033[1mCopying the script to the nautilus-python folder...\033[0m\n"
 tryDoing sudo mkdir -p "$extensions_folder"
-tryDoing sudo cp -i "${selfpath%/*}/metanautilus.py" "$extensions_folder"
+tryDoing sudo cp -n "${selfpath%/*}/metanautilus.py" "$extensions_folder"
 
 printf "\033[1mGiving \033[3mexecute permission\033[0;0m\033[1m to the script...\033[0m\n"
 tryDoing sudo chmod +x "$extensions_folder/metanautilus.py"
 
+# finishing
 printf "\n\033[2m[press any key to restart Nautilus]\033[0m "; read -n 1 -s; printf "\n\n"
-tryDoing sudo rm -fr "$HOME/.cache/metanautilus" &> /dev/null
 tryDoing mkdir "$HOME/.cache/metanautilus"; fi
 nautilus -q
 nautilus &> /dev/null &
